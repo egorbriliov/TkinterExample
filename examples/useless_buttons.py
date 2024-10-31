16ebb5b8a78907ca038b30f41afb6e9cd8ee22ae
egorbriliov <egor.briliov@gmail.com>
2024-10-31 17:19:49 +0300

    File was reworked

--- examples/useless_buttons.py
+++ examples/useless_buttons.py
@@ -1,15 +1,47 @@
-# Importing necessary imports
 from tkinter import *
 
 
-def main():
-    # Creating basic window
+def root() -> Tk:
+    """
+    :return: Tk
+    """
     root: Tk = Tk()
-    # Setting settings
     root.title("It's me, mark")
-    root.geometry("200x5")
-    # Starts program
-    root.mainloop()
+    root.geometry("200x200")
+    return root
+
+def create_buttons(app: Frame) -> None:
+    """
+    Creates buttons inside Frame
+    :return: None
+    """
+    # Creates new button | First example to creating
+    bttn1: Button = Button(app, text="I do nothing!")
+    bttn1.grid()
+
+    # Creates new button | Second example to creating
+    bttn2: Button = Button(app)
+    bttn2.grid()
+    # Changing name after creating | First example
+    bttn2.configure(text="Me to!")
+
+    # Creates new button | Third example to creating
+    bttn3: Button = Button(app)
+    bttn3.grid()
+    # Changing name after creating | Second example
+    bttn3["text"] = "And me"
+
+
+def main():
+    r: Tk = root()
+
+    app: Frame = Frame(r)
+    app.grid()
+
+    # Creates buttons inside Frame
+    create_buttons(app=app)
+
+    r.mainloop()
 
 if __name__ == "__main__":
     main()
